5297a5ee841452950a32c8f7e8e03a03421ba0ad
McRaZick <mcrazick@mail.com>
2020-03-18 21:00:20 +0000

    Profile settings
    User is now able to update their details, such as name, lastname, email and the username.

--- mybakes/settings.py
+++ mybakes/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'recipes.apps.RecipesConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
+    'django_cleanup',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,37 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
+
+"""
+Fields based on the following:
+https://webarchive.nationalarchives.gov.uk/+/http://www.cabinetoffice.gov.uk/media/254290/GDS%20Catalogue%20Vol%202.pdf
+"""
 
 
 class UserRegisterForm(UserCreationForm):
-	# Field forms: https://webarchive.nationalarchives.gov.uk/+/http://www.cabinetoffice.gov.uk/media/254290/GDS%20Catalogue%20Vol%202.pdf
 	email = forms.EmailField(max_length=150)
 	first_name = forms.CharField(max_length=35, label='Imię')
-	last_name = forms.CharField(max_length=35, label='Naswisko')
+	last_name = forms.CharField(max_length=35, label='Nazwisko')
 
 	class Meta:
 		model = User
 		fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+	email = forms.EmailField(max_length=150)
+	first_name = forms.CharField(max_length=35, label='Imię')
+	last_name = forms.CharField(max_length=35, label='Nazwisko')
+
+	class Meta:
+		model = User
+		fields = ['username', 'first_name', 'last_name', 'email']
+
+
+
+class ProfileUpdateForm(forms.ModelForm):
+	class Meta:
+		model = Profile
+		fields = ['image']
