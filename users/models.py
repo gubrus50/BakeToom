88217aad74b872a0527ea44a0df855e89c7d62f9
McRaZick <mcrazick@mail.com>
2020-03-18 22:44:33 +0000

    Registry Issue
    Users were not able to register due to error:  "save() got an unexpected keyword argument 'force_insert'". Issue has been resolved.

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from PIL import Image
 
 # Create your models here.
 
+
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	image = models.ImageField(default='default.jpg', upload_to='profile_pics')
@@ -11,8 +12,8 @@ class Profile(models.Model):
 	def __str__(self):
 		return f'{self.user.username} Profile'
 
-	def save(self):
-		super().save()
+	def save(self, *args, **kwargs):
+		super().save(*args, **kwargs)
 
 		img = Image.open(self.image.path)
 
