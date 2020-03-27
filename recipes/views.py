18783bf9b0cc636f0131dd2fcf02d4dd945750d1
McRaZick <mcrazick@mail.com>
2020-03-27 19:09:43 +0000

    Miror design fix
    Improved the display of profile page (template) on mobile version.

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import (
+	ListView,
+	DetailView,
+	CreateView
+)
 from django.utils.translation import gettext_lazy as _
 from django.http import HttpResponse
 from django.contrib import messages
@@ -41,6 +45,7 @@ class RecipeCreateView(LoginRequiredMixin, CreateView):
 	fields = ['title', 'image', 'description', 'method']
 
 
+
 	"""
 	def post(self, *args, **kwargs):
 		if 'title' in self.request.POST and 'category' in self.request.POST:
@@ -59,11 +64,11 @@ class RecipeCreateView(LoginRequiredMixin, CreateView):
 					)
 
 				messages.success(self.request, f'Your recipe has been successfully created')
+	"""
 
 	#return HttpResponse('ERROR')
-	"""
+
 	def form_valid(self, form):
 		form.instance.author = self.request.user
 		messages.success(self.request, f'Your recipe has been successfully created')
 		return super().form_valid(form)
-
