47e42ecf8bb5aaf75c0d810ff47c07dc444fe666
McRaZick <mcrazick@mail.com>
2020-04-18 00:22:04 +0100

    Search bar filter design
    Filter icon can be found in the search bar. Once pressed, a container with many options will appear.
    
    Options are not functional (Yet due to sudden wifi difficulties).
    
    Now I have to include new options for the recipe-form template, create validation script for the views + recipe model and then include filters functionality.

--- recipes/static/recipes/js/main.js
+++ recipes/static/recipes/js/main.js
@@ -45,7 +45,6 @@ window.addEventListener('scroll', scrollFunc);
 
 
 
-
 $(document).ready(function()
 {
 	$('#scroll-to-top-button').hide();
@@ -62,5 +61,39 @@ $(document).ready(function()
 	$("ignorecookies, .acceptcookies").click(function() {
 		setCookie('hide_cookies_popup',true,1)
 		$("#cookieConsent").fadeOut(200)
+	});
+
+	/* Search filter container toggle animation */
+	var fc = $('div[name="filter-container"]');
+	$('#search-filter-btn').on('click', function() {
+		if ($(fc).is(':hidden')) {
+			$(fc).slideDown('slow')
+		}
+		else {
+			$('div[name="filter-container"]').slideUp('slow')
+		}
 	})
+
+	/* Search filter -> Disable upload date checkbox is publish/edit date is checked */
+	// jquery :checked feature was bugged at 18/04/2020, thats why I use selector by class
+	var elm = document.getElementsByClassName('filter-search-by-date');
+	var pd = elm[0];
+	var ed = elm[1];
+	
+	function disableOrEnableUploadDateFilter() {
+		if (pd.checked || ed.checked) {
+			// disable upload date radio buttons from search filter-container
+			$('ul[name="upload-date-children"]').children('li').each(function(index) {
+				$(this).children('input[type="radio"]').prop('disabled', true)
+			})
+		} else {
+			// enable upload date radio buttons from search filter-container
+			$('ul[name="upload-date-children"]').children('li').each(function(index) {
+				$(this).children('input[type="radio"]').prop('disabled', false)
+			})
+		}
+	}
+	
+	pd.onclick = function() { disableOrEnableUploadDateFilter() }
+	ed.onclick = function() { disableOrEnableUploadDateFilter() }
 })
