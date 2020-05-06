00a56d4f355693e0c016c3d87b52f5dfc35cf0ff
McRaZick <mcrazick@mail.com>
2020-05-06 23:43:37 +0100

    Updated Settings.py

--- baketoom/settings.py
+++ baketoom/settings.py
@@ -81,10 +81,17 @@ WSGI_APPLICATION = 'baketoom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
 }
 
