b0c53cbc24080b81ae3e771a59d24d005fb59d83
McRaZick <mcrazick@mail.com>
2020-05-07 19:27:27 +0100

    Set Debug Value

--- baketoom/settings.py
+++ baketoom/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('BAKETOOM_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('BAKETOOM_DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['baketoom.herokuapp.com']
 
